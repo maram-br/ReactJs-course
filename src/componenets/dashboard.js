9724e093733d5fd59644e9c800b33c8c40d296ab
maram <maram.benrhouma@insat.ucar.tn>
2024-06-26 13:39:27 +0100

    .

---
+++ src/componenets/dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './navbar';
+function Dashboard() {
+  return (
+    <React.Fragment>
+    <Navbar />
+    <div>
+        this is dashboard
+    </div>
+    </React.Fragment>
+  );
+}
+
+export default Dashboard;
---
+++ src/componenets/sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <div>
+        this is sidebar 
+    </div>
+  );
+}
+
+export default Sidebar;
